a04df0d36103df3ccb51ac022e3788da5d10ba85
Muhammad Asim <quickbooks2018@gmail.com>
2023-05-21 07:25:12 +0000

    terraform locals

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
+locals {
+  # if workspace is default do not create vpc
+  # if workspace is dev create vpc with name dev-vpc
+  # if workspace is qa create vpc with name qa-vpc
+
+  # below vpc_name means if workspace is default then vpc_name = default
+  vpc_name = "${terraform.workspace == "dev" ? "vpc-dev" : "vpc-qa"}"
+}
+
 resource "aws_vpc" "vpc" {
-  # 0 do not create vpc in default workspace #off
-  # 1 create vpc in all other workspaces     #on
+  # 0 do not create vpc in default workspace #(do not create) true
+  # 1 create vpc in all other workspaces     #(create)        else
   count                = terraform.workspace == "default" ? 0 : 1
   cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name        = var.vpc_name
+    Name        = local.vpc_name
     Environment = terraform.workspace
     location    = "Pakistan"
   }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
-  default     = "10.10.0.0/16"
+  default     = ""
   type        = string
 }
 
